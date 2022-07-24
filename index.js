4033e4a8b211839a166794fa0d4c789d055d6deb
Theodore Boersma <theo@Theodores-Mac-mini.local>
2022-07-23 20:47:32 -0700

    Updated for youtube

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
-const e = require('express');
-
 
 require('./passport');
 
@@ -19,88 +17,74 @@ app.use(express.static(path.join(__dirname, 'static/images')))
 app.use(bodyParser.urlencoded({ extended: true, limit: '80mb', parameterLimit: 100000 }));
 app.use(express.json());
 app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
 
 
 // Routing
 app.get('/logout', function (req, response) {
-  if (req.session.loggedin) {
-    req.session.destroy();
-
-    response.redirect('/');
-    response.end()
-  } else {
-    // Not logged in
-    response.redirect('/');
-    response.end()
-
-  }
-
+    if (req.session.loggedin) {
+        req.session.destroy();
+        response.redirect("/");
+        response.end()
+    } else {
+        response.redirect("/");
+        response.end()
+    }
 });
 
 
 app.get('/welcome', function (req, response) {
-  if (req.session.loggedin) {
-    response.sendFile(path.join(__dirname, 'static/displayEmail.html'));
-
-  } else {
-    response.write("access denied")
-    response.end()
-  }
-
+    if (req.session.loggedin) {
+        response.sendFile(path.join(__dirname, "static/displayEmail.html"))
+    } else {
+        response.write("access denied");
+        response.end();
+    }
 });
 
 app.get('/getData', function (req, response) {
-  if (req.session.loggedin) {
-    var data = req.session.userData 
-    response.write(JSON.stringify({ data }))
-    response.end()
-  } else {
-    response.write("access denied")
-    response.end()
-  }
-
+    if (req.session.loggedin) {
+        var data = JSON.stringify(req.session.userData);
+        response.write(data);
+        response.end();
+    } else {
+        response.write("access denied");
+        response.end();
+    }
 });
 
 
 
 app.get('/', function (req, response) {
-if (req.session.loggedin) {
-    response.redirect('/welcome');
-    response.end()
-  } else {
-    response.sendFile(path.join(__dirname, 'static/login.html'));
-  }
-
+    if (req.session.loggedin) {
+        response.redirect("/welcome")
+        response.end();
+    } else {
+        response.sendFile(path.join(__dirname, "static/login.html"));
+    }
 });
 
-app.get('/google',
-  passport.authenticate('google', {
-    scope:
-      ['email', 'profile']
-  }
-  ));
+app.get('/google', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}));
 
-app.get('/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: '/',
-  }),
-  function (req, response) {
-      req.session.userData = {
+app.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: "/",
+}), function(req,response){
+    req.session.userData = {
         profile: req.user.photos[0].value,
         email: req.user.email
-      }
-      req.session.loggedin = true;
-      
-      response.redirect("/welcome")
-      response.end()
+    };
+    req.session.loggedin = true;;
 
-  });
+    response.redirect("/welcome");
+    response.end();
+});
 
 server.listen(port, function () {
-  console.log('Starting server on port ' + port);
+    console.log('Starting server on port ' + port);
 });
 console.clear();
--- passport.js
+++ passport.js
@@ -1,21 +1,21 @@
-const passport =require("passport")
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 passport.serializeUser(function(user, done) {
     done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-        done(null, user);
+    done(null, user);
 });
 
 passport.use(new GoogleStrategy({
-        clientID:"108138655449-673udglt076bha8jh1jkrbauak2pgngv.apps.googleusercontent.com",
-        clientSecret:"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",
-        callbackURL: "http://localhost:500/google/callback",
-        passReqToCallback   : true
-    },
-    function(request, accessToken, refreshToken, profile, done) {
-            return done(null, profile);
-    }
+    clientID:"<client-id>",
+    clientSecret:"<client-secret>",
+    callbackURL: "http://localhost:500/google/callback",
+    passReqToCallback   : true
+},
+function(request, accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+}
 ));
